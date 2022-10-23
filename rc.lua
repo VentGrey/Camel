d3cd46697b4b777604259c74cb1a210c80dbb49d
VentGrey <omarpurataf@gmail.com>
2022-10-23 00:53:10 -0500

    Change taglist icons to match epitaph ones

--- rc.lua
+++ rc.lua
@@ -173,7 +173,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "🏠", "🌎", "⚡️", "🎧", "🐱", "📖", "💠", "📝", "🎮" }, s, awful.layout.layouts[1])
+    awful.tag({ "", "", "", "", "", "", "", "", "" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
